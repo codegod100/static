c1268d1a1637a077ab5a393ed5f97fe2f7b4e2b1
Your Name <you@example.com>
2024-06-20 10:11:34 +0000

    config

--- _cms.ts
+++ _cms.ts
@@ -1,3 +1,18 @@
-import cms from "blog/_cms.ts";
+import lumeCMS from "lume/cms/mod.ts";
+import GitHub from "lume/cms/storage/github.ts";
+import { Octokit } from "npm:octokit";
+
+const cms = lumeCMS();
+
+cms.storage(
+    "src",
+    new GitHub({
+        client: new Octokit({ auth: Deno.env.get("GITHUB_TOKEN") }),
+        owner: "username",
+        repo: "example",
+    }),
+);
+
+// Rest of the configuration....
 
 export default cms;
